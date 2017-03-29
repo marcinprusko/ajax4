4adfb823d123ab0477c3bc4ef32b0d15f4a1d3eb
marcinprusko <marcinprusko@gmail.com>
2017-03-29 22:46:20 +0200

    ajax4

---
+++ js/App.js
@@ -0,0 +1,50 @@
+GET /board
+-----------------------------
+Response:
+{
+   id: int,
+   name: string,
+   columns: [{
+       id: int,
+       name: string,
+       cards: [{
+           id: int,
+           bootcamp_kanban_column_id: int,
+           name: string
+       }]
+   }]
+}
+
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': <[X-Client-Id]>,
+  'X-Auth-Token': <[X-Auth-Token]>
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(card);
+  	})
+}
